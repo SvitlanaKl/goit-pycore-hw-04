332bb6241d9095d671257c656b6346fea3822d61
svitlanakl <sv.klepka@gmail.com>
2024-05-06 23:33:56 +0300

    task4 added

---
+++ HW-04_part4/main.py
@@ -0,0 +1,36 @@
+# Напишіть консольного бота помічника, який розпізнаватиме команди, що вводяться з клавіатури,
+# та буде відповідати відповідно до введеної команди
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    try:
+        name, phone = args
+        contacts[name] = phone
+        return "Contact added."
+    except ValueError:
+        print("Invalid number of arguments. Expected: name, phone.")
+        return "Failed to add contact."
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
