3d96115b34092d71e672a84caf0c1d2af351e2a5
svitlanakl <sv.klepka@gmail.com>
2024-05-06 23:17:04 +0300

    Renamed part 1,2; Added part 3

--- HW-04_part3/main.py
+++ HW-04_part3/main.py
@@ -7,9 +7,24 @@
 # Повинна бути перевірка та обробка помилок, наприклад, якщо вказаний шлях не існує або він не веде до директорії.
 
 import sys
+import os
 
 from colorama import Fore, Style
+from pathlib import Path
 
+def list_directory_contents(dir_path, indent=0):
+    try:
+        dir_path = Path(dir_path)
+        for entry in dir_path.iterdir():
+            if entry.is_dir():
+                print(Fore.BLUE + "  " * indent + entry.name + "/" + Style.RESET_ALL)
+                list_directory_contents(entry, indent + 1)
+            else:
+                print(Fore.GREEN + "  " * indent + entry.name + Style.RESET_ALL)
+    except FileNotFoundError:
+        print("Директорію не знайдено.")
+    except PermissionError:
+        print("Відсутні права доступу до директорії.")
 
-
+list_directory_contents("C:\Users\WORK\Desktop\GoIT\Repository\Python\HomeWork\goit-pycore-hw-04>")
 
